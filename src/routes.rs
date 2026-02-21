28e3737147ef1b68479d24fa47e2148318e00a32
Lukas Pustina <lukas@pustina.de>
2026-02-21 18:18:42 +0100

    Bump version to 0.7.0

--- src/routes.rs
+++ src/routes.rs
@@ -23,7 +23,7 @@ use crate::state::AppState;
     info(
         title = "ifconfig-rs",
         description = "IP address lookup and enrichment API",
-        version = "0.6.0",
+        version = "0.7.0",
         license(name = "MIT"),
     ),
     paths(
